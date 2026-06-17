a33abbb7f4bac13253b5a98f7eaacb60bfff4a8f
Mahi <mahidixit0903@gmail.com>
2026-06-18 00:50:57 +0530

    ReverseString.java

---
+++ Strings/ReverseString.java
@@ -0,0 +1,18 @@
+class Solution {
+     public  void reverseString(char[] s) {
+        int low = 0;
+        int high = s.length-1;
+
+        while(low <= high) {
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+
+            low++;
+            high--;
+        }
+    }
+}
+
+Time Complexiy = O(n)
+Space Complexity = O(1)
