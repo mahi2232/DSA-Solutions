c340ab389b5828351a3de75b1cd3e8692fe6b3ac
Mahi <mahidixit0903@gmail.com>
2026-06-18 15:45:39 +0530

    Create DeleteDuplicates.java

---
+++ LinkedList/DeleteDuplicates.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+
+        while(temp != null && temp.next != null) {
+            if(temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
+
+
+Time Complexity = O(n)
+Space Complexity = O(1)
