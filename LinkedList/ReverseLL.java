c1eec6fde575d1904b39e164e647951666a5a403
Mahi <mahidixit0903@gmail.com>
2026-06-20 20:27:25 +0530

    Create ReverseLL.java

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        
+        while(curr != null) {
+            ListNode nxt = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+}
+
+Time Complexity = O(n)
+Space Complexity = O(1)
