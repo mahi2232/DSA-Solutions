732abc7da023337109905666007385dca883f9b9
Mahi <mahidixit0903@gmail.com>
2026-06-21 19:38:58 +0530

    Create ValidParentheses.java

---
+++ Stack/ValidParentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if (st.isEmpty()) return false;
+
+                if (ch == ')' && st.peek() == '(' ||
+                    ch == '}' && st.peek() == '{' ||
+                    ch == ']' && st.peek() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
+
+
+Time Complexity = O(n);
+Space Complexity = O(n)
