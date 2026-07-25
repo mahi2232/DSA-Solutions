c0378df0f251f164f9920800a0b57f04ea064800
Mahi <mahidixit0903@gmail.com>
2026-07-25 23:25:51 +0530

    Create CourseSchedule2.java

---
+++ Graphs/CourseSchedule2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] pre : prerequisites) {
+            adj.get(pre[1]).add(pre[0]);
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            for(int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        int[] topo = new int[numCourses];
+        int i = 0;
+        int cnt = 0;
+        while(!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo[i++] = node;
+            cnt++;
+            for(int it : adj.get(node)) {
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        if(cnt != numCourses) return new int[]{};
+        return topo;
+    }
+}
+
+Time Complexity = O(v+e)
+Space Complexity = O(v+e)
